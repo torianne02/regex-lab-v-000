707f215be31895f029fa4ab4740b301321b399fa
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-27 19:10:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.preg_grep(/^[A-Za-z]{5}/i, $array)
+  text.preg_grep(/^[A-Za-z]{5}/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
